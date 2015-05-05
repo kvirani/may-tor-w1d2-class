a80ae76791f60e924510f5858b47a88fde67f1c1
Khurram Virani <kvirani@gmail.com>
2015-05-05 11:11:56 -0400

    refactored a bit, and tested

--- example1.rb
+++ example1.rb
@@ -1,13 +1,37 @@
-number = rand(10) + 1
-puts "The real number was #{number}"
+# def something
+#   puts "fdsfs"
+# end
 
-puts "Please guess a number bw 1 and 10, werd? "
-guess = gets.chomp
+# Genereate a number
+# Ask and retreive their guess
+# Compare the two
 
+def generate_number(min, max)
+  return rand(max - min + 1) + min
+end
+
+def get_guess
+  puts "Please guess a number bw 1 and 10, werd? "
+  return gets.chomp.to_i
+end
+
+def correct_guess?(num, answer)
+  return num == answer
+end
+
+def evaluate_guess(answer, number)
+  if correct_guess?(number, answer)
+    puts "NAILED IT DAWG!"
+  else
+    puts "NOPE"
+  end
+end
+
+number = generate_number(1, 10)
+puts "The real number was #{number}" # DEBUG CODE
+
+guess = get_guess
 puts "You guessed #{guess}"
 
-if number == guess
-  puts "NAILED IT DAWG!"
-else
-  puts "NOPE"
-end
+evaluate_guess(guess, number)
+
